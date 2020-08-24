ce14d56a7747c405b1488babb6f232fff4e8745e
ABHINAV RANJAN <40801086+mygitt1@users.noreply.github.com>
2020-08-24 11:40:05 +0530

    Add files via upload

---
+++ models/image_data.dart
@@ -0,0 +1,15 @@
+class ImageData {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final int height;
+  final int width;
+
+  ImageData({
+    this.height,
+    this.id,
+    this.imageUrl,
+    this.title,
+    this.width,
+  });
+}
